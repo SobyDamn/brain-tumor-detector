b7232fa275fb015e578f39fa4aff3dbf92ab66ea
Sritabh Priyadarshi <sobydanny@gmail.com>
2022-06-21 18:06:53 +0530

    result page added

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import classifier
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/result',methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        #get image from form data
+        img = request.files['scan_file']
+        #Classify the image
+        predictions = classifier.classify(img)
+        #First key is the result
+        result = list(predictions.keys())[0]
+        return render_template("result.html",result = result)
+    return "Error"
+
 
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
